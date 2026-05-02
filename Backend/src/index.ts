2298f1623653bb5c8bf21d1ad153575b8963bffc
Ashish <ashish187.be22@chitkara.edu.in>
2026-05-02 11:33:56 +0530

    cors fix

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URL || "");
 const JWT_PASSWORD = process.env.JWT_SECRET;
 const app = express();
 app.use(cors({
-    origin: "https://brain-link-rho.vercel.app/",
+    origin: "https://brain-link-rho.vercel.app",
    credentials: true
 })) ;
 
